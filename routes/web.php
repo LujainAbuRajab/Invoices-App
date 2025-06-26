97671fef440521e84f74f92c2761c3bbdf78518b
Lujain <lujainaburajab@gmail.com>
2025-06-26 12:07:30 +0300

    set the authentication - Done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/{page}', 'App\Http\Controllers\AdminController@index');
+
